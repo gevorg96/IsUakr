1408a185a3d83241e853b44a2cbd09c411ce8bf0
gkesyan <gevorgkesyan@gmail.com>
2019-11-02 18:00:27 +0300

    initial

--- IsUakr/Program.cs
+++ IsUakr/Program.cs
@@ -8,15 +8,7 @@ namespace IsUakr
     {
         static void Main(string[] args)
         {
-            var builder = new PostgreSqlConnectionStringBuilder(
-                "d1adtvm6bavqp6",
-                "ec2-174-129-253-62.compute-1.amazonaws.com",
-                "8600d78661ce0ad3417bf056d64a807a5fbf6abc37091f05a99b53bb3dc25a13",
-                "uqyvgpqzzrgslm",
-                5432, 
-                true, 
-                true, 
-                SslMode.Require);
+            
             
             using (NpgDbContext db = new NpgDbContext(builder.ConnectionString))
             {
